9e3155af508a099f391a1636ad6ff37ca5424269
Konstantin Zaitsev <classicsvell@gmail.com>
2021-11-03 00:00:31 +0300

    saga

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ const app = express()
 require('dotenv').config()
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.send('Not today')
+})
 
 app.listen(process.env.PORT || 3000)
