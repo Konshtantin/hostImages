055e636f6ca63a84eff098ce240f982bf29bd668
Konstantin Zaitsev <classicsvell@gmail.com>
2021-11-02 23:51:28 +0300

    Sec

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-
-const app = express()
-require('dotenv').config()
-app.use(express.urlencoded({extended: true}))
-app.use(express.static('public'))
-
-app.listen(process.env.PORT || 3000)
